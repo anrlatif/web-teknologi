fc31a27db34af5d7d1f3cf0fe597a27d468c2813
Latif <latifnakbtn@gmail.com>
2025-11-08 17:27:41 +0700

    UPDATE LOGIN/LAYOUT

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,14 +1,14 @@
 import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
 
-export default function RootLayout({
+export default function LoginLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
+    <div className={`${lusitana.className} antialiased`}>
+      {children}
+    </div>
   );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 import {
